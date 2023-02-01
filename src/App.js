4e0394e9bb6b164b5a78dd08f28af62df32e80b5
tealvin <tealvinsjhi11ly@gmail.com>
2023-02-01 20:20:57 +0800

    set the basic setting of react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
+import CategoryPage from "./pages/CategoryPage";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage />,
+    },
+    {
+      path: "/category",
+      element: <CategoryPage />,
+    },
+  ]);
+
   return (
     <ChakraProvider>
-      <HomePage />
+      <RouterProvider router={router} />
     </ChakraProvider>
   );
 }
